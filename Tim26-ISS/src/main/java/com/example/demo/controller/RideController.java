6cb44df7d878a8319d8adf24efa6b12ad69cb7b4
Danilo-Babic <daniloraska@gmail.com>
2022-12-11 15:58:39 +0100

    Added create ride
    Added post method for creating a ride

--- Tim26-ISS/src/main/java/com/example/demo/controller/RideController.java
+++ Tim26-ISS/src/main/java/com/example/demo/controller/RideController.java
@@ -1,7 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.DriverRideOverDTO;
+import com.example.demo.dto.RejectionDTO;
+import com.example.demo.dto.RideRequestDTO;
+import com.example.demo.dto.RideResponseDTO;
 import com.example.demo.service.RideService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +20,13 @@ public class RideController {
     @Autowired
     RideService rideService;
 
-
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<RideResponseDTO> createRide(@RequestBody RideRequestDTO ride){
+        RideResponseDTO created = new RideResponseDTO();
+        created.setDriver(new DriverRideOverDTO());
+        created.setPassengers(ride.getPassengers());
+        created.setLocations(ride.getLocations());
+        created.setRejection(new RejectionDTO());
+        return new ResponseEntity<RideResponseDTO>(created, HttpStatus.OK);
+    }
 }
