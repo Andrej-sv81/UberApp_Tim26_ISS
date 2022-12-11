b2ac5250ab092a7cff7f7016407c9fa4c69d6c89
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2022-12-11 09:38:34 +0100

    User Rides Get

--- Tim26-ISS/src/main/java/com/example/demo/controller/UserController.java
+++ Tim26-ISS/src/main/java/com/example/demo/controller/UserController.java
@@ -1,17 +1,18 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.dto.MultipleRidesDTO;
 import com.example.demo.dto.MultipleUsersDTO;
 import com.example.demo.dto.UserResponseDTO;
+import com.example.demo.dto.RideDTO;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,8 +24,10 @@ public class UserController {
     UserService userService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<MultipleUsersDTO> getUsers(@RequestParam(required = false)Integer page, @RequestParam(required = false)Integer size){
-        //Collection<Passenger> passengers = passengerService.findAll();
+    public ResponseEntity<MultipleUsersDTO> getUsers(
+            @RequestParam(required = false)Integer page,
+            @RequestParam(required = false)Integer size){
+
         List<UserResponseDTO> users = new ArrayList<UserResponseDTO>();
         for(int i=0;i<10;i++){
             UserResponseDTO dummy = new UserResponseDTO();
@@ -35,5 +38,28 @@ public class UserController {
         response.setResults(users);
         response.setTotalCount(users.size());
         return new ResponseEntity<MultipleUsersDTO>(response, HttpStatus.OK);
+    }
+    @GetMapping(value = "/{id}/ride", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MultipleRidesDTO> getRides(
+            @PathVariable(value = "id", required = true) Integer id,
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) Integer size,
+            @RequestParam(required = false) String sort,
+            @RequestParam(required = false) LocalDateTime from,
+            @RequestParam(required = false) LocalDateTime to
+
+            ){
+        //TODO: Napraviti ispravne mockup podatke za sve 'dubine' da bi se prikazala struktura
+        MultipleRidesDTO rides = new MultipleRidesDTO();
+        List<RideDTO> rideList = new ArrayList<>();
+        for(int i =0; i<10; i++){
+            RideDTO dummy = new RideDTO();
+            dummy.setId(i);
+            rideList.add(dummy);
+        }
+
+        rides.setResults(rideList);
+
+        return new ResponseEntity<MultipleRidesDTO>(rides, HttpStatus.OK);
     }
 }
---
+++ Tim26-ISS/src/main/java/com/example/demo/dto/MultipleRidesDTO.java
@@ -0,0 +1,39 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+public class MultipleRidesDTO {
+    private Integer totalCount;
+    private List<RideDTO> results;
+
+    public MultipleRidesDTO(){};
+
+    public MultipleRidesDTO(Integer totalCount, List<RideDTO> results) {
+        this.totalCount = totalCount;
+        this.results = results;
+    }
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(Integer totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public List<RideDTO> getResults() {
+        return results;
+    }
+
+    public void setResults(List<RideDTO> results) {
+        this.results = results;
+    }
+
+    @Override
+    public String toString() {
+        return "MultipleRidesDTO{" +
+                "totalCount=" + totalCount +
+                ", results=" + results +
+                '}';
+    }
+}
