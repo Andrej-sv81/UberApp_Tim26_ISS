d42b690678c0622d929c0595a53000b116a1aa88
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2022-12-11 19:11:03 +0100

    Vehicle Controller

--- Tim26-ISS/src/main/java/com/example/demo/model/Vehicle.java
+++ Tim26-ISS/src/main/java/com/example/demo/model/Vehicle.java
@@ -3,5 +3,51 @@ package com.example.demo.model;
 
 
 public class Vehicle {
+
+    private String address;
+
+    private double latitude;
+
+    private double longitude;
     public enum vehicleType {STANDARDNO};
+
+    public Vehicle(){};
+    public Vehicle(String address, double latitude, double longitude) {
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "address='" + address + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
 }
