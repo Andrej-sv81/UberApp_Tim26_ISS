6aa5d60fb7464aaad013a45eff814c49fe37e9e8
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2022-12-11 11:56:29 +0100

    User Note Post

--- Tim26-ISS/src/main/java/com/example/demo/dto/UserNoteRequestDTO.java
+++ Tim26-ISS/src/main/java/com/example/demo/dto/UserNoteRequestDTO.java
@@ -1,4 +1,25 @@
 package com.example.demo.dto;
 
 public class UserNoteRequestDTO {
+    private String message;
+
+    public UserNoteRequestDTO(){};
+    public UserNoteRequestDTO(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "UserNoteRequestDTO{" +
+                "message='" + message + '\'' +
+                '}';
+    }
 }
