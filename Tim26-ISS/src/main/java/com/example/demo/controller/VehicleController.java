c68d56601729752472c4e7d5bfe091be0ac100d2
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2023-01-15 18:01:33 +0100

    Vehicle Controller

--- Tim26-ISS/src/main/java/com/example/demo/controller/VehicleController.java
+++ Tim26-ISS/src/main/java/com/example/demo/controller/VehicleController.java
@@ -1,10 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.HttpStatusMessageDTO;
 import com.example.demo.dto.VehicleLocationRequestDTO;
+import com.example.demo.model.Location;
+import com.example.demo.model.User;
+import com.example.demo.model.Vehicle;
+import com.example.demo.service.LocationService;
 import com.example.demo.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,10 +21,33 @@ public class VehicleController {
     @Autowired
     VehicleService vehicleService;
 
-    @PutMapping(value="/{id}/location", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseStatusException changeLocation(@PathVariable(value = "id", required = true) Integer id,
-                                                  @RequestBody VehicleLocationRequestDTO request) throws Exception{
+    @Autowired
+    LocationService locationService;
 
-        return new ResponseStatusException(HttpStatus.NO_CONTENT);
+    @PutMapping(value="/{id}/location", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> changeLocation(@PathVariable(value = "id", required = true) Integer id,
+                                                  @RequestBody VehicleLocationRequestDTO request){
+        Vehicle vehicle = vehicleService.findOneById(id);
+        if(vehicle == null){
+            HttpStatusMessageDTO httpStatusMessageDTO = new HttpStatusMessageDTO("Vehicle does not exist!");
+            return new ResponseEntity<HttpStatusMessageDTO>(httpStatusMessageDTO, HttpStatus.NOT_FOUND);
+        }
+
+        Location locationOld = vehicle.getLocation();
+        if(locationOld!=null){
+            locationOld.setAddress(request.getAddress());
+            locationOld.setLatitude(request.getLatitude());
+            locationOld.setLongitude(request.getLongitude());
+            locationService.save(locationOld);
+            vehicle.setLocation(locationOld);
+        }else{
+            Location location = new Location(request);
+            locationService.save(location);
+            vehicle.setLocation(location);
+            vehicleService.save(vehicle);
+        }
+
+        HttpStatusMessageDTO response = new HttpStatusMessageDTO("Coordinates successfully updated");
+        return new ResponseEntity<HttpStatusMessageDTO>(response, HttpStatus.NO_CONTENT);
     }
 }
--- Tim26-ISS/src/main/java/com/example/demo/service/interfaces/IVehicleService.java
+++ Tim26-ISS/src/main/java/com/example/demo/service/interfaces/IVehicleService.java
@@ -1,4 +1,9 @@
 package com.example.demo.service.interfaces;
 
+import com.example.demo.model.Vehicle;
+
 public interface IVehicleService {
+    Vehicle findOneById(Integer id);
+
+    void save(Vehicle vehicle);
 }
