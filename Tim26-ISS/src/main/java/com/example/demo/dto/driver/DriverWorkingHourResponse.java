f23fe483c3502b1844e2f95fde2f57bca07d801b
Danilo-Babic <daniloraska@gmail.com>
2023-02-05 16:22:39 +0100

    Added working hour methods
    TODO: all working hours and all driver rides

--- Tim26-ISS/src/main/java/com/example/demo/dto/driver/DriverWorkingHourResponse.java
+++ Tim26-ISS/src/main/java/com/example/demo/dto/driver/DriverWorkingHourResponse.java
@@ -1,4 +1,61 @@
 package com.example.demo.dto.driver;
 
+import com.example.demo.model.WorkingHour;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class DriverWorkingHourResponse {
+    private Integer id;
+    private String start;
+    private String end;
+
+    public DriverWorkingHourResponse() {
+    }
+
+    public DriverWorkingHourResponse(Integer id, String start, String end) {
+        this.id = id;
+        this.start = start;
+        this.end = end;
+    }
+
+    public DriverWorkingHourResponse(WorkingHour created){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        this.id = created.getId();
+        this.start = dateFormat.format(created.getStartTime());      //TODO formatiraj lepo ovaj string
+        this.end = dateFormat.format(created.getEndTime());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "DriverWorkingHourResponse{" +
+                "id=" + id +
+                ", start='" + start + '\'' +
+                ", end='" + end + '\'' +
+                '}';
+    }
 }
